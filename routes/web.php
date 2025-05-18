698ddea74c635c09a92c471638af7f1e41b01d8f
qwerty6588 <abbosyakhiyaev@gmail.com>
2025-05-18 23:12:44 +0500

    ff

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/telegram', function () {
+    try {
+        Telegram::sendMessage([
+            'chat_id' => env('TELEGRAM_CHAT_ID'),
+            'text' => 'Laravel-бот ',
+        ]);
+        return '✅ The message was sent successfully .';
+    } catch (\Exception $e) {
+        return '❌ Error: ' . $e->getMessage();
+    }
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
